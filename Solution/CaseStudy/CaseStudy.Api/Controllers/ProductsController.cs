a901e32af6f9456c86c64a2e125a51c4fddd1850
Semih Hanoglu <hanogluu@gmail.com>
2024-04-05 00:43:43 +0300

    test(controllerApi) : Product Add - Get Test

---
+++ Solution/CaseStudy/CaseStudy.Api/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using CaseStudy.Application.Repositories;
+using CaseStudy.Domain.Entities;
+using CaseStudy.Persistence.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseStudy.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        readonly private IProductReadRepository _productReadRepository;
+        readonly private IProductWriteRepository _productWriteRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository , IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            await _productWriteRepository.AddAsync(new Product { Name = "Test" , CreatedDate = DateTime.Now });
+            await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
+        }
+
+    }
+}
