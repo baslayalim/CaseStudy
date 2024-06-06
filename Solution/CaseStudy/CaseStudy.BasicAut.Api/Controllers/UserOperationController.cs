92ae39c231f04ae6fadde545cd8e2dc78d12eb33
Semih Hanoglu <hanogluu@gmail.com>
2024-06-06 21:13:06 +0300

    feat(basicAuthentication) : Basic Authentication Development

---
+++ Solution/CaseStudy/CaseStudy.BasicAut.Api/Controllers/UserOperationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseStudy.BasicAut.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserOperationController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            return Task.FromResult<IActionResult>(Ok("UserOperationGet"));
+        }
+    }
+}
---
+++ Solution/CaseStudy/CaseStudy.BasicAut.Api/Controllers/UserProfilController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseStudy.BasicAut.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfilController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IActionResult> GetProfil()
+        {
+            return Task.FromResult<IActionResult>(Ok("UserProfil"));
+        }
+
+        [HttpGet("Detail")]
+        public Task<IActionResult> GetDetail()
+        {
+            return Task.FromResult<IActionResult>(Ok("UserProfilDetail"));
+        }
+    }
+}
