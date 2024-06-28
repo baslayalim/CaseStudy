dd934da4fed773574134aafd39b3b1f9c5db79c4
Semih Hanoglu <hanogluu@gmail.com>
2024-06-28 15:18:54 +0300

    fix(unitOfWorkError) : Unit Of Work Error

--- Solution/CaseStudy/CaseStudy.Api/Controllers/UnitOfWorkController.cs
+++ Solution/CaseStudy/CaseStudy.Api/Controllers/UnitOfWorkController.cs
@@ -34,6 +34,9 @@ namespace CaseStudy.Api.Controllers
             Add(new Product { Id = new Guid(), Name = "Mevlana Şekeri", Product_Description = ".", Stock = 5 });
             Add(new Product { Id = new Guid(), Name = "Samsung Yazıcı", Product_Description = ".", Stock = 5 });
 
+
+            Add(new Customer { Id = new Guid(), Name = "Feyza", SurName = "HANOĞLU"  , Hobi = "Yüzmek , Koşmak , Uçmak , Zıplamak , Yürümek , Hareket Etmek" });
+
             Commit();
 
             return true;
